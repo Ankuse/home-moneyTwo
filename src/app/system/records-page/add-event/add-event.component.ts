38c9762e6e9c950315c0831d05c0e23ec942f5cb
Ankuse <Esukna@gmail.com>
2018-06-11 09:32:04 +0300

    (fix-bug-type)

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
-  categories: Array;
+  categories: Array<object>;
 
   constructor() { }
 
